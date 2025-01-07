e26056f9e1ca152a4c925cb3a81203fb17f88f67
Ray Anthony <62559681+DollieSon@users.noreply.github.com>
2025-01-08 05:33:48 +0800

    fixed bug on NN construction/new function

--- BNN/src/main.rs
+++ BNN/src/main.rs
@@ -5,8 +5,5 @@ pub mod neural_net;
 fn main() {
     let mut NN = NeuralNet::new(2, 2, 1);
     let x = 0.65;
-    println!("{:?}", sigmoid(&x));
-    println!("hidden: {:?}", NN.hidden_layer);
-    println!("input: {:?}", NN.input_layer);
-    println!("output: {:?}", NN.output_size);
+    NN.train(&vec![1.0, 0.0], &vec![1.0]);
 }
--- BNN/src/neural_net.rs
+++ BNN/src/neural_net.rs
@@ -6,13 +6,13 @@ pub fn sigmoid(x: &f64) -> f64 {
     return 1.0 / (1.0 + EXP.powf(-x));
 }
 
-struct Node {
+struct NeuronNode {
     bias: f64,
     weights: Vec<f64>,
     act_val: f64,
 }
 
-impl Node {
+impl NeuronNode {
     fn new(nex_nodes: u32) -> Self {
         let mut rand_gen = rand::thread_rng();
 
@@ -20,23 +20,30 @@ impl Node {
         for _ in 0..nex_nodes {
             weights.push(rand_gen.gen());
         }
-        return Node {
+        return NeuronNode {
             bias: 0.0,
             weights: weights,
             act_val: 0.0,
         };
     }
+
     fn reset_act(mut self) {
         self.act_val = 0.0;
+    }
+    fn activate(&mut self, prev_Nodes: &Vec<NeuronNode>, neuron_index: usize) {
+        let mut activated_value = self.bias;
+        for (index, neuron) in prev_Nodes.iter().enumerate() {
+            activated_value += neuron.act_val * (neuron.weights.get(neuron_index).unwrap());
+        }
+        self.act_val = activated_value;
     }
 }
 
 pub struct NeuralNet {
     // Input -> input_layer -> hident node -> hidden_layer -> output
-    pub input_layer: Vec<Node>,
-    pub hidden_layer: Vec<Node>,
-    pub output_size: u32,
-    //
+    pub input_layer: Vec<NeuronNode>,
+    pub hidden_layer: Vec<NeuronNode>,
+    pub output_layer: Vec<NeuronNode>, //
 }
 
 impl NeuralNet {
@@ -44,31 +51,49 @@ impl NeuralNet {
         if input == 0 || hidden == 0 || output == 0 {
             panic!("NN::new Cannot be zero!!");
         }
-        let input_vec: Vec<Node> = (0..input).map(|_| Node::new(hidden)).collect();
-        let hidden_vec: Vec<Node> = (0..input).map(|_| Node::new(output)).collect();
-
+        let input_vec: Vec<NeuronNode> = (0..input).map(|_| NeuronNode::new(hidden)).collect();
+        let hidden_vec: Vec<NeuronNode> = (0..hidden).map(|_| NeuronNode::new(output)).collect();
+        let output_vec: Vec<NeuronNode> = (0..output).map(|_| NeuronNode::new(1)).collect();
         return NeuralNet {
             input_layer: input_vec,
             hidden_layer: hidden_vec,
-            output_size: output,
+            output_layer: output_vec,
         };
-
-        // for n in 0..input {
-        //     input_vec.push(rand_gen.gen());
-        // }
-        // for n in 0..hidden {
-        //     hidden_vec.push(rand_gen.gen());
-        // }
-        // return NeuralNet {
-        //     input_layer: input_vec,
-        //     hidden_layer: hidden_vec,
-        //     output_size: output,
-        // };
     }
-    pub fn train(&self, input: &Vec<f64>, output: &Vec<f64>) {
-        if input.len() != self.input_layer.len() || output.len() != output.len() {
-            panic!("Training: Vector Sizes are Inoconsistent");
+    // we assume that input is valid as well as output
+    pub fn train(&mut self, input: &Vec<f64>, output: &Vec<f64>) {
+        // setting input nodes
+        println!("Activating Input Layer");
+        self.input_layer
+            .iter_mut()
+            .enumerate()
+            .for_each(|(index, neuron)| neuron.act_val = *input.get(index).unwrap());
+
+        self.input_layer
+            .iter_mut()
+            .enumerate()
+            .for_each(|(index, neuron)| println!("{}", neuron.act_val));
+        //activating hidden nodes
+        println!("Activating Hidden Layer");
+        self.hidden_layer
+            .iter_mut()
+            .enumerate()
+            .for_each(|(index, neuron)| neuron.activate(&self.input_layer, index));
+
+        self.hidden_layer
+            .iter_mut()
+            .enumerate()
+            .for_each(|(index, neuron)| println!("{}", neuron.act_val));
+        //calc output
+        println!("{}", self.output_layer.len());
+        println!("Activating Output Layer");
+        self.output_layer
+            .iter_mut()
+            .enumerate()
+            .for_each(|(index, neuron)| neuron.activate(&self.hidden_layer, index));
+        //print output
+        for neuron in self.output_layer.iter() {
+            println!("{}", neuron.act_val);
         }
-        let input_res: Vec<f64> = Vec::new();
     }
 }
